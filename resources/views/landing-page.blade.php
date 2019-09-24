69ffb5a994bd9ec97ca7d0937420a64574f14f70
MAHMOUD <emahmoud@beltonefinancial.com>
2019-09-24 18:49:17 +0200

    finish video 12 part 11

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -20,17 +20,13 @@
         <header class="with-background" style="background-color: #535353">
             <div class="top-nav container">
                 <div class="logo">Laravel Ecommerce</div>
-                <ul>
-                    <li><a href="{{ route('shop.index') }}">Shop</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Blog</a></li>
-                    <li><a href="{{ route('cart.index') }}">Cart <span class="cart-count"><span>3</span></span></a></li>
-                </ul>
+                {{ menu('main', 'partials.menus.main') }}
             </div> <!-- end top-nav -->
             <div class="hero container">
                 <div class="hero-copy">
                     <h1>Laravel Ecommerce Demo</h1>
                     <p>Includes multiple products, categories, a shopping cart and a checkout system with Stripe integration.</p>
+
                     <div class="hero-buttons">
                         <a href="www.linkedin.com/in/eman-muhamed" class="button button-white">Screencast</a>
                         <a href="https://github.com/emaanmohamed" class="button button-white">GitHub</a>
