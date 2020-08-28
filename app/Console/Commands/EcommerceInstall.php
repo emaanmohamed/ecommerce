5c2e11711e5ee72562a9c7b55379fcb19f349cfb
emaanmohamed <eman.mohamed.ka@gmail.com>
2020-08-28 17:21:21 +0200

    log

---
+++ app/Console/Commands/EcommerceInstall.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+
+class EcommerceInstall extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ecommerce:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Install dummy data';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $this->callSilent('storage:link');
+        File::copyDirectory(public_path('img/products'), public_path('storage/products/dummy'));
+    }
+}
