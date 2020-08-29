02e9e97ea9b8c2c7124d34deee5b5d83015e2287
emaanmohamed <eman.mohamed.ka@gmail.com>
2020-08-29 16:23:47 +0200

    bugs in vidoe 18

--- app/User.php
+++ app/User.php
@@ -26,4 +26,10 @@ class User extends \TCG\Voyager\Models\User
     protected $hidden = [
         'password', 'remember_token',
     ];
+    
+    public function orders()
+    {
+    	return $this->hasMany('App\Order');
+    }
+    
 }
---
+++ database/migrations/2020_08_29_114419_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->reference('id')->on('users')->onUpdate('cascade')->onDelete('set null');
+            $this->string('billing_email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
